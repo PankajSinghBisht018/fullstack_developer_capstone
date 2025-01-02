d007fdd63c1b7f148cf370290ce64ed0a4c55c9c
Pankaj Singh Bisht <pankajsbisht88@gmail.com>
2025-01-02 03:40:22 -0500

    Fix linting errors

---
+++ server/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Make migrations and migrate the database
+echo "Making migrations and migrating the database."
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Execute the provided command (e.g., gunicorn)
+exec "$@"
